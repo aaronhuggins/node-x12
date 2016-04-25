6b821395b4243920da74a6d6aa6d6cd61f99dd16
Josh Johnson <JoshJ@highjump.com>
2016-04-24 21:23:36 -0400

    wip: change envelope to segment representation

--- src/X12Interchange.ts
+++ src/X12Interchange.ts
@@ -2,182 +2,46 @@
 
 import { Range } from './Positioning';
 import { X12FunctionalGroup } from './X12FunctionalGroup';
+import { X12Segment } from './X12Segment';
+import { defaultSerializationOptions, X12SerializationOptions } from './X12SerializationOptions';
 
 export class X12Interchange {
     constructor(segmentTerminator: string, elementDelimiter: string) {
         this.functionalGroups = new Array<X12FunctionalGroup>();
         this.segmentTerminator = segmentTerminator;
         this.elementDelimiter = elementDelimiter;
-        this.headerRange = new Range();
-        this.trailerRange = new Range();
     }
     
-    authorizationInfoQualifier: string;
-    authorizationInfo: string;
-    securityInfoQualifier: string;
-    securityInfo: string;
-    senderQualifier: string;
-	senderId: string;
-	receiverQualifier: string;
-	receiverId: string;
-	controlVersionNumber: string;
-	controlNumber: string;
-    interchangeDate: string;
-    interchangeTime: string;
-    repetitionSeparator: string;
-    acknowledgementRequested: boolean;
-    usageIndicator: string;
-    componentElementSeparator: string;
+    header: X12Segment;
+    trailer: X12Segment;
     
     functionalGroups: X12FunctionalGroup[];
     
     segmentTerminator: string;
     elementDelimiter: string;
     
-    headerRange: Range;
-    trailerRange: Range;
-    
-    toString(format?: boolean, endOfLine?: string): string {
-        endOfLine = endOfLine || '\n';
+    toString(options?: X12SerializationOptions): string {
+        options = defaultSerializationOptions(options);
         
         if (this.segmentTerminator === '\n') {
-            endOfLine = '';
+            options.endOfLine = '';
         }
         
-        let edi = 'ISA';
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.authorizationInfoQualifier, 2);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.authorizationInfo, 10);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.securityInfoQualifier, 2);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.securityInfo, 10);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.senderQualifier, 2);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.senderId, 15);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.receiverQualifier, 2);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.receiverId, 15);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.interchangeDate, 6);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.interchangeTime, 4);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.repetitionSeparator, 1);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.controlVersionNumber, 5);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.controlNumber, 9);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.acknowledgementRequested ? '1' : '0', 1);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.usageIndicator, 1);
-        edi += this.elementDelimiter;
-        edi += this._padRight(this.componentElementSeparator, 1);
-        edi += this.segmentTerminator;
+        let edi = this.header.toString(options);
         
-        if (format) {
-            edi += endOfLine;
+        if (options.format) {
+            edi += options.endOfLine;
         }
         
         for (let i = 0; i < this.functionalGroups.length; i++) {
-            let group = this.functionalGroups[i];
+            edi += this.functionalGroups[i].toString(options);
             
-            edi += 'GS';
-            edi += this.elementDelimiter;
-            edi += group.functionalIdentifierCode;
-            edi += this.elementDelimiter;
-            edi += group.senderCode;
-            edi += this.elementDelimiter;
-            edi += group.receiverCode;
-            edi += this.elementDelimiter;
-            edi += group.date;
-            edi += this.elementDelimiter;
-            edi += group.time;
-            edi += this.elementDelimiter;
-            edi += group.controlNumber;
-            edi += this.elementDelimiter;
-            edi += group.agencyCode;
-            edi += this.elementDelimiter;
-            edi += group.version;
-            edi += this.segmentTerminator;
-            
-            if (format) {
-                edi += endOfLine;
-            }
-            
-            for (let j = 0; j < group.transactions.length; j++) {
-                let transaction = group.transactions[i];
-                
-                edi += 'ST';
-                edi += this.elementDelimiter;
-                edi += transaction.transactionSet;
-                edi += this.elementDelimiter;
-                edi += transaction.controlNumber;
-                
-                if (transaction.conventionReference) {
-                    edi += this.elementDelimiter;
-                    edi += transaction.conventionReference;
-                }
-                
-                edi += this.segmentTerminator;
-                
-                if (format) {
-                    edi += endOfLine;
-                }
-                
-                for (let k = 0; k < transaction.segments.length; k++) {
-                    let segment = transaction.segments[k];
-                    
-                    edi += segment.tag;
-                    
-                    for (let l = 0; l < segment.elements.length; l++) {
-                        let element = segment.elements[l];
-                        
-                        edi += this.elementDelimiter;
-                        edi += element.value;
-                    }
-                    
-                    edi += this.segmentTerminator;
-                    
-                    if (format) {
-                        edi += endOfLine;
-                    }
-                }
-                
-                edi += 'SE';
-                edi += this.elementDelimiter;
-                edi += transaction.segments.length + 2;
-                edi += this.elementDelimiter;
-                edi += transaction.controlNumber;
-                edi += this.segmentTerminator;
-                
-                if (format) {
-                    edi += endOfLine;
-                }
-            }
-            
-            edi += 'GE';
-            edi += this.elementDelimiter;
-            edi += group.transactions.length;
-            edi += this.elementDelimiter;
-            edi += group.controlNumber;
-            edi += this.segmentTerminator;
-            
-            if (format) {
-                edi += endOfLine;
+            if (options.format) {
+                edi += options.endOfLine;
             }
         }
         
-        edi += 'IEA';
-        edi += this.elementDelimiter;
-        edi += this.functionalGroups.length;
-        edi += this.elementDelimiter;
-        edi += this.controlNumber;
-        edi += this.segmentTerminator;
+        edi += this.trailer.toString(options);
         
         return edi;
     }
--- src/X12Segment.ts
+++ src/X12Segment.ts
@@ -3,6 +3,7 @@
 import { Range } from './Positioning';
 import { X12Transaction } from './X12Transaction';
 import { X12Element } from './X12Element';
+import { defaultSerializationOptions, X12SerializationOptions } from './X12SerializationOptions';
 
 export class X12Segment {
     constructor() {
@@ -14,6 +15,21 @@ export class X12Segment {
     tag: string;
     elements: X12Element[];
     range: Range;
+    
+    toString(options?: X12SerializationOptions): string {
+        options = defaultSerializationOptions(options);
+        
+        let edi = this.tag;
+        
+        for (let i = 0; i < this.elements.length; i++) {
+            edi += options.elementDelimiter;
+            edi += this.elements[i].value;
+        }
+        
+        edi += options.segmentTerminator;
+        
+        return edi;
+    }
     
     valueOf(segmentPosition: number, defaultValue?: string): string {
         let index = segmentPosition - 1;

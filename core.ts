60867a8a56c10f87f5f2cbd03ca124e2895f4710
Aaron Huggins <ahuggins@nethealthshops.com>
2019-09-06 20:35:54 -0500

    Added generator class export

--- core.ts
+++ core.ts
@@ -2,9 +2,10 @@
 
 export * from './src/X12Element';
 export * from './src/X12FunctionalGroup';
+export * from './src/X12Generator';
 export * from './src/X12Interchange';
 export * from './src/X12Parser';
+export * from './src/X12QueryEngine';
 export * from './src/X12Segment';
 export * from './src/X12Transaction';
-export * from './src/X12QueryEngine';
 export * from './src/X12SerializationOptions';
--- dist/core.js
+++ dist/core.js
@@ -5,9 +5,10 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./src/X12Element"));
 __export(require("./src/X12FunctionalGroup"));
+__export(require("./src/X12Generator"));
 __export(require("./src/X12Interchange"));
 __export(require("./src/X12Parser"));
+__export(require("./src/X12QueryEngine"));
 __export(require("./src/X12Segment"));
 __export(require("./src/X12Transaction"));
-__export(require("./src/X12QueryEngine"));
 __export(require("./src/X12SerializationOptions"));

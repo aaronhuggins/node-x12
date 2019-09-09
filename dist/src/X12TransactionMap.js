13d789e553562a9b9bf6c7666463a8a969f791f7
Aaron Huggins <ahuggins@nethealthshops.com>
2019-09-09 10:47:54 -0500

    Added foreach macro evaluation to transaction map

--- dist/src/X12TransactionMap.js
+++ dist/src/X12TransactionMap.js
@@ -14,38 +14,92 @@ class X12TransactionMap {
     toObject(map) {
         map = map || this.map;
         const clone = JSON.parse(JSON.stringify(map));
+        let clones = new Array();
         const engine = new X12QueryEngine_1.X12QueryEngine(false);
         const interchange = new X12Interchange_1.X12Interchange();
         interchange.setHeader(['00', '', '00', '', 'ZZ', '00000000', '01', '00000000', '000000', '0000', '|', '00000', '00000000', '0', 'P', '>']);
         interchange.addFunctionalGroup().transactions = [this.transaction];
-        Object.keys(clone).forEach((key) => {
+        Object.keys(map).forEach((key) => {
             if (Object.prototype.hasOwnProperty.call(map, key)) {
-                if (Array.isArray(clone[key]) && typeof clone[key][0] === 'string') {
+                if (Array.isArray(map[key]) && typeof map[key][0] === 'string') {
                     const newArray = new Array();
-                    clone[key].forEach((query) => {
+                    map[key].forEach((query) => {
                         try {
-                            newArray.push(engine.querySingle(interchange, query).value);
+                            const result = engine.querySingle(interchange, query);
+                            if (result === null) {
+                                newArray.push(null);
+                            }
+                            else if (result.value === null || Array.isArray(clone[key][0])) {
+                                if (result.value) {
+                                    clone[key].forEach((array) => {
+                                        array.push(result.value);
+                                    });
+                                }
+                                else {
+                                    let superArray = new Array();
+                                    if (Array.isArray(clone[key][0])) {
+                                        superArray = clone[key];
+                                    }
+                                    result.values.forEach((value, index) => {
+                                        if (!Array.isArray(superArray[index])) {
+                                            superArray[index] = new Array();
+                                        }
+                                        superArray[index].push(value);
+                                    });
+                                    newArray.push(...superArray);
+                                }
+                            }
+                            else {
+                                newArray.push(result.value);
+                            }
                         }
                         catch (err) {
-                            throw new Errors_1.QuerySyntaxError(`${err.message}; bad query in ${clone[key]}`);
+                            throw new Errors_1.QuerySyntaxError(`${err.message}; bad query in ${map[key]}`);
                         }
                     });
                     clone[key] = newArray;
                 }
-                else if (typeof clone[key] === 'string') {
+                else if (typeof map[key] === 'string') {
                     try {
-                        clone[key] = engine.querySingle(interchange, clone[key]).value;
+                        clone[key] = engine.querySingle(interchange, map[key]).value;
+                        const result = engine.querySingle(interchange, map[key]);
+                        if (result === null) {
+                            clone[key] = null;
+                        }
+                        else if (result.value === null || Array.isArray(clones)) {
+                            if (result.value) {
+                                clones.forEach((cloned) => {
+                                    cloned[key] = result.value;
+                                });
+                            }
+                            else {
+                                if (!Array.isArray(clones)) {
+                                    clones = new Array();
+                                }
+                                result.values.forEach((value, index) => {
+                                    if (clones[index] === undefined) {
+                                        clones[index] = JSON.parse(JSON.stringify(clone));
+                                    }
+                                    clones[index][key] = result.value;
+                                });
+                            }
+                        }
+                        else {
+                            clone[key] = result.value;
+                        }
                     }
                     catch (err) {
-                        throw new Errors_1.QuerySyntaxError(`${err.message}; bad query in ${clone[key]}`);
+                        throw new Errors_1.QuerySyntaxError(`${err.message}; bad query in ${map[key]}`);
                     }
                 }
                 else {
-                    clone[key] = this.toObject(clone[key]);
+                    clone[key] = this.toObject(map[key]);
                 }
             }
         });
-        return clone;
+        return Array.isArray(clones)
+            ? clones
+            : clone;
     }
 }
 exports.X12TransactionMap = X12TransactionMap;
--- src/X12TransactionMap.ts
+++ src/X12TransactionMap.ts
@@ -22,38 +22,93 @@ export class X12TransactionMap {
         map = map || this.map;
 
         const clone = JSON.parse(JSON.stringify(map));
+        let clones = new Array<object>();
         const engine = new X12QueryEngine(false);
         const interchange = new X12Interchange();
         interchange.setHeader(['00','','00','','ZZ','00000000','01','00000000','000000','0000','|','00000','00000000','0','P','>']);
         interchange.addFunctionalGroup().transactions = [this.transaction];
 
-        Object.keys(clone).forEach((key) => {
+        Object.keys(map).forEach((key) => {
 
             if (Object.prototype.hasOwnProperty.call(map, key)) {
-                if (Array.isArray(clone[key]) && typeof clone[key][0] === 'string') {
-                    const newArray = new Array<string>();
+                if (Array.isArray(map[key]) && typeof map[key][0] === 'string') {
+                    const newArray = new Array<string | string[]>();
 
-                    (clone[key] as Array<string>).forEach((query) => {
+                    (map[key] as Array<string>).forEach((query) => {
                         try {
-                            newArray.push(engine.querySingle(interchange, query).value);
+                            const result = engine.querySingle(interchange, query);
+
+                            if (result === null) {
+                                newArray.push(null);
+                            } else if (result.value === null || Array.isArray(clone[key][0])) {
+                                if (result.value) {
+                                    clone[key].forEach((array: Array<string>) => {
+                                        array.push(result.value)
+                                    })
+                                } else {
+                                    let superArray = new Array<string[]>();
+
+                                    if (Array.isArray(clone[key][0])) {
+                                        superArray = clone[key]
+                                    }
+
+                                    result.values.forEach((value: string, index: number) => {
+                                        if (!Array.isArray(superArray[index])) {
+                                            superArray[index] = new Array<string>();
+                                        }
+
+                                        superArray[index].push(value)
+                                    })
+
+                                    newArray.push(...superArray);
+                                }
+                            } else {
+                                newArray.push(result.value);
+                            }
                         } catch (err) {
-                            throw new QuerySyntaxError(`${err.message}; bad query in ${clone[key]}`);
+                            throw new QuerySyntaxError(`${err.message}; bad query in ${map[key]}`);
                         }
                     })
 
                     clone[key] = newArray;
-                } else if (typeof clone[key] === 'string') {
+                } else if (typeof map[key] === 'string') {
                     try {
-                        clone[key] = engine.querySingle(interchange, clone[key]).value;
+                        clone[key] = engine.querySingle(interchange, map[key]).value;
+                        const result = engine.querySingle(interchange, map[key]);
+
+                        if (result === null) {
+                            clone[key] = null;
+                        } else if (result.value === null || Array.isArray(clones)) {
+                            if (result.value) {
+                                clones.forEach((cloned: Array<object>) => {
+                                    cloned[key] = result.value;
+                                })
+                            } else {
+                                if (!Array.isArray(clones)) {
+                                    clones = new Array<object>();
+                                }
+                                result.values.forEach((value: string, index: number) => {
+                                    if (clones[index] === undefined) {
+                                        clones[index] = JSON.parse(JSON.stringify(clone));
+                                    }
+
+                                    clones[index][key] = result.value
+                                })
+                            }
+                        } else {
+                            clone[key] = result.value
+                        }
                     } catch (err) {
-                        throw new QuerySyntaxError(`${err.message}; bad query in ${clone[key]}`);
+                        throw new QuerySyntaxError(`${err.message}; bad query in ${map[key]}`);
                     }
                 } else {
-                    clone[key] = this.toObject(clone[key]);
+                    clone[key] = this.toObject(map[key]);
                 }
             }
         })
 
-        return clone;
+        return Array.isArray(clones)
+            ? clones
+            : clone;
     }
 }

2ab11cdcdd70062f8f07d3939bd8c47203d7e395
Aaron Huggins <ahuggins@nethealthshops.com>
2019-09-16 09:34:31 -0500

    Added xunit test output for mocha and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,62 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
+const fs = require('fs')
+const gulp = require('gulp')
+const shell = require('gulp-shell')
+
+gulp.task('mkdir', async (done) => {
+  if (!fs.existsSync('coverage')) {
+    fs.mkdirSync('coverage')
+  }
+
+  return done()
+})
 
 gulp.task('compile', shell.task([
-    'tsc'
-]));
+  'tsc'
+]))
 
 gulp.task('compile:docs', shell.task([
-    'jsdoc2md --no-cache --files ./src/*.ts --configure ./jsdoc2md.json > ./docs/API.md'
-]));
+  'jsdoc2md --no-cache --files ./src/*.ts --configure ./jsdoc2md.json > ./docs/API.md'
+]))
 
 gulp.task('clean:dist', shell.task([
-    'del-cli ./dist'
-]));
+  'del-cli ./dist'
+]))
 
-gulp.task('test', shell.task([
-    'mocha'
-]));
+gulp.task('mocha', shell.task([
+  'mocha'
+]))
 
-gulp.task('test:coverage', shell.task([
-    'nyc mocha'
-]));
+gulp.task('mocha:coverage', shell.task([
+  'nyc mocha'
+]))
+
+gulp.task('mocha:xunit', shell.task([
+  'nyc mocha --reporter=xunit --reporter-options output=./coverage/mocha.xml'
+]))
 
 gulp.task('eslint', shell.task([
-    'eslint --ext .ts .'
-]));
+  'eslint --ext .ts .'
+]))
 
 gulp.task('eslint:fix', shell.task([
-    'eslint --ext .ts --fix .'
-]));
+  'eslint --ext .ts --fix .'
+]))
+
+gulp.task('eslint:xunit', shell.task([
+  'eslint --format junit --ext .ts . > ./coverage/eslint.xml',
+], {
+  ignoreErrors: true
+}))
+
+gulp.task('test', gulp.parallel(
+  gulp.series(
+    'mkdir',
+    'eslint:xunit'
+  ),
+  'mocha:xunit'
+))
+
+gulp.task('test:local', gulp.parallel(
+  'eslint',
+  'mocha:coverage'
+))

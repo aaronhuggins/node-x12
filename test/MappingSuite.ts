2fe413980fa8a788c0c960a97df44447ce77783e
Aaron Huggins <ahuggins@nethealthshops.com>
2020-08-03 15:21:48 -0500

    Fix object comparision in test

--- test/MappingSuite.ts
+++ test/MappingSuite.ts
@@ -4,6 +4,7 @@ import 'mocha'
 import { X12Parser, X12Transaction, X12TransactionMap, X12Interchange } from '../core'
 
 import fs = require('fs')
+import assert = require('assert')
 
 const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
 const mapJson = fs.readFileSync('test/test-data/850_map.json', 'utf8')
@@ -18,11 +19,8 @@ describe('X12Mapping', () => {
     const interchange = parser.parse(edi) as X12Interchange
     const transaction = interchange.functionalGroups[0].transactions[0]
     const mapper = new X12TransactionMap(JSON.parse(mapJson), transaction)
-    const result = JSON.stringify(mapper.toObject())
 
-    if (result !== resultJson) {
-      throw new Error(`Formatted JSON does not match source. Found ${result}, expected ${resultJson}.`)
-    }
+    assert.deepStrictEqual(mapper.toObject(), JSON.parse(resultJson))
   })
 
   it('should map data to transaction with custom macro', () => {

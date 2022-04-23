bd3f5ca58ac3f1510c350ad8351dbb99397f8502
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-23 17:01:00 -0500

    Fix test suite for x12 query

--- test/QuerySuite.ts
+++ test/QuerySuite.ts
@@ -1,12 +1,11 @@
 'use strict'
 
-import 'mocha'
-import { X12Parser, X12QueryEngine } from '../core'
-import * as fs from 'fs'
+import "https://raw.githubusercontent.com/aaronhuggins/deno_mocha/e6c179156821c626354a8c792518958625078a82/global_mocha.ts";
+import { X12Parser, X12QueryEngine } from '../core.ts'
 
 describe('X12QueryEngine', () => {
   it('should handle basic element references', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'REF02')
@@ -17,7 +16,7 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle qualified element references', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'REF02:REF01["DP"]')
@@ -30,7 +29,7 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle segment path element references', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'PO1-PID05:PID01["F"]')
@@ -41,7 +40,7 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle HL path element references', () => {
-    const edi = fs.readFileSync('test/test-data/856.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/856.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'HL+S+O+I-LIN03')
@@ -52,7 +51,7 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle HL paths where HL03 is a number', () => {
-    const edi = fs.readFileSync('test/test-data/271.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/271.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'HL+20+21+22-NM101')
@@ -63,45 +62,45 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle FOREACH macro references', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const result = engine.querySingle(edi, 'FOREACH(PO1)=>PID05:PID01["F"]')
 
-    if (result.values.length !== 6) {
+    if (result?.values.length !== 6) {
       throw new Error(
-        `Expected six matching elements for FOREACH(PO1)=>PID05:PID01["F"]; received ${result.values.length}.`
+        `Expected six matching elements for FOREACH(PO1)=>PID05:PID01["F"]; received ${result?.values.length}.`
       )
     }
   })
 
   it('should handle CONCAT macro references', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const result = engine.querySingle(edi, 'CONCAT(REF02:REF01["DP"], & )=>REF02:REF01["PS"]')
 
-    if (result.value !== '038 & R') {
-      throw new Error(`Expected '038 & R'; received '${result.value}'.`)
+    if (result?.value !== '038 & R') {
+      throw new Error(`Expected '038 & R'; received '${result?.value}'.`)
     }
   })
 
   it('should return valid range information for segments and elements', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const result = engine.querySingle(edi, 'BEG03')
 
-    if (result.segment.range.start.line !== 3) {
-      throw new Error(`Start line for segment is incorrect; found ${result.segment.range.start.line}, expected 3.`)
+    if (result?.segment?.range.start.line !== 3) {
+      throw new Error(`Start line for segment is incorrect; found ${result?.segment?.range.start.line}, expected 3.`)
     }
 
     if (result.segment.range.start.character !== 0) {
       throw new Error(`Start char for segment is incorrect; found ${result.segment.range.start.character}, expected 0.`)
     }
 
-    if (result.element.range.start.line !== 3) {
-      throw new Error(`Start line for element is incorrect; found ${result.element.range.start.line}, expected 3.`)
+    if (result?.element?.range.start.line !== 3) {
+      throw new Error(`Start line for element is incorrect; found ${result?.element?.range.start.line}, expected 3.`)
     }
 
     if (result.element.range.start.character !== 10) {
@@ -128,13 +127,13 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle envelope queries', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'ISA06')
 
     if (results.length === 1) {
-      if (results[0].value.trim() !== '4405197800') {
+      if (results[0]?.value?.trim() !== '4405197800') {
         throw new Error(`Expected 4405197800, found ${results[0].value}.`)
       }
     } else {
@@ -143,24 +142,24 @@ describe('X12QueryEngine', () => {
   })
 
   it('should handle queries for files with line feed segment terminators', () => {
-    const edi = fs.readFileSync('test/test-data/850_2.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850_2.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const result = engine.querySingle(edi, 'REF02:REF01["DP"]')
 
-    if (result.value.trim() !== '038') {
-      throw new Error(`Expected 038, found ${result.value}.`)
+    if (result?.value?.trim() !== '038') {
+      throw new Error(`Expected 038, found ${result?.value}.`)
     }
   })
 
   it('should handle chained qualifiers', () => {
-    const edi = fs.readFileSync('test/test-data/850.edi', 'utf8')
+    const edi = Deno.readTextFileSync('test/test-data/850.edi')
     const parser = new X12Parser(true)
     const engine = new X12QueryEngine(parser)
     const results = engine.query(edi, 'REF02:REF01["DP"]:BEG02["SA"]')
 
     if (results.length === 1) {
-      if (results[0].value.trim() !== '038') {
+      if (results[0]?.value?.trim() !== '038') {
         throw new Error(`Expected 038, found ${results[0].value}.`)
       }
     } else {

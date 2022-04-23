5bc720684d0d9d8a15a0a484c812dec3d392e867
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-23 16:16:08 -0500

    Fix imports for core suite

--- test/CoreSuite.ts
+++ test/CoreSuite.ts
@@ -1,9 +1,9 @@
 'use strict'
 
-import 'mocha'
-import { ArgumentNullError, GeneratorError, ParserError, QuerySyntaxError } from '../src/Errors'
-import { X12Diagnostic } from '../src/X12Diagnostic'
-import * as core from '../core'
+import "https://raw.githubusercontent.com/aaronhuggins/deno_mocha/e6c179156821c626354a8c792518958625078a82/global_mocha.ts";
+import { ArgumentNullError, GeneratorError, ParserError, QuerySyntaxError } from '../src/Errors.ts'
+import { X12Diagnostic } from '../src/X12Diagnostic.ts'
+import * as core from '../core.ts'
 
 describe('X12Core', () => {
   it('should export members', () => {

43b551d90d0298667475d955b2eda8f653863284
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-24 16:40:43 -0500

    Format build script and remove files property

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -6,7 +6,7 @@ import { copy } from "https://deno.land/std@0.136.0/fs/mod.ts";
 import { inc as increment } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.22.0/mod.ts";
 
-const NPM_NAME = "node-x12"
+const NPM_NAME = "node-x12";
 
 await emptyDir("./npm");
 await copy("test/test-data", "npm/esm/test/test-data", { overwrite: true });
@@ -60,25 +60,23 @@ await build({
     "https://deno.land/std@0.136.0/node/stream.ts": "stream",
     "https://deno.land/std@0.136.0/node/string_decoder.ts": "string_decoder",
     "https://deno.land/std@0.136.0/node/crypto.ts": "crypto",
-    "https://cdn.skypack.dev/pin/liquidjs@v9.37.0-dA2YkE2JlVe1VjIZ5g3G/mode=imports/optimized/liquidjs.js": {
-      name: "liquidjs",
-      version: "^9.37.0"
-    }
+    "https://cdn.skypack.dev/pin/liquidjs@v9.37.0-dA2YkE2JlVe1VjIZ5g3G/mode=imports/optimized/liquidjs.js":
+      {
+        name: "liquidjs",
+        version: "^9.37.0",
+      },
   },
   package: {
     name: NPM_NAME,
     version: newVersion,
-    description: "ASC X12 parser, generator, query engine, and mapper; now with support for streams.",
+    description:
+      "ASC X12 parser, generator, query engine, and mapper; now with support for streams.",
     keywords: [
       "x12",
       "edi",
       "ansi",
       "asc",
-      "ecommerce"
-    ],
-    files: [
-      "src/**",
-      "mod.js"
+      "ecommerce",
     ],
     homepage: `https://github.com/aaronhuggins/${NPM_NAME}#readme`,
     bugs: `https://github.com/aaronhuggins/${NPM_NAME}/issues`,
